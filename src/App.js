270febf94ce01e482ba775efcda8fde0cff49d16
Suhas Bhairav <suhasbhairav@gmail.com>
2021-05-15 16:31:33 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import React, {useState, useEffect, useRef} from 'react';
 import './App.css';
 
 function App() {
+
+  const [country, setCountry] = useState('');
+  const inputRef = useRef('');
+  const [random, setRandom] = useState(-1);
+
+
+  const resetCountryName = () => {
+    setCountry('');
+    
+  };
+
+  useEffect(() => {
+    inputRef.current = random;
+  }, [random]);
+
+  const generateRandomNumber = () => {
+    setRandom(Math.random() * 100);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+      Country Name: <input type="text" name="country" value={country} onChange={e => setCountry(e.target.value)}/>
+      <button onClick={resetCountryName}>Reset Country</button>
+      <div>The country name:{country}</div>
+      
+        <div>Random Number: {random}</div>
+      
+      <button onClick={generateRandomNumber}>Generate</button>
+      { typeof inputRef.current !== undefined && 
+        <div>Previous Number: {inputRef.current}</div>
+      }
     </div>
   );
 }
